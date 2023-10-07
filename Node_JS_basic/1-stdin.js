327f7cc9ccfa2a1af3fc9128efbecc1afaecf9c3
xiayudev <jctuesta7@gmail.com>
2023-10-06 19:01:40 -0500

    fix: add new line feed

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -9,5 +9,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
