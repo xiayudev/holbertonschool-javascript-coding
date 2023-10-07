bf7cd47e2094b75bf01dc471d8e8bf85385e790c
xiayudev <jctuesta7@gmail.com>
2023-10-06 21:24:21 -0500

    feat: read a file sync

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function countStudents(fileName) {
+  fs.readFile(fileName, (error, data) => {
+    if (error) {
+      throw new Error('Cannot load the database');
+    } else {
+      let students = data.toString().split('\n').map((elem) => elem.split(','));
+      students = students.slice(1, students.length - 1); // Array of array of students
+      const fields = {}; // All fields availables
+      students.forEach((student) => {
+        fields[student[student.length - 1]] = (fields[student[student.length - 1]] || 0) + 1;
+      });
+
+      console.log(`Number of students: ${students.length}`);
+      for (const field in fields) {
+        if (field) {
+          // Filter just the firts names of the students
+          const result = students.filter((std) => std[std.length - 1] === field).map((el) => el[0]);
+          console.log(`Number of students in ${field}: ${result.length}. List: ${result.join(', ')}`);
+        }
+      }
+    }
+  });
+}
+
+module.exports = countStudents;
