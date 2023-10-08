374c4e7b7f7bef7599901fd9a9015c9f23996aa8
xiayudev <jctuesta7@gmail.com>
2023-10-07 19:02:15 -0500

    fix: handle error

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -12,33 +12,39 @@ const app = http.createServer((req, res) => {
   } else if (req.url === '/students') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    let students = [];
-    const out = [];
-    fs.readFile(process.argv.length === 2 ? 'database.csv' : process.argv[2], (error, data) => {
-      if (error) {
-        console.log(error);
-      } else {
-        students = data.toString().split('\n').map((elem) => elem.split(','));
-        students = students.slice(1, students.length - 1); // Array of array of students
-        const fields = {}; // All fields availables
-        students.forEach((student) => {
-          fields[student[student.length - 1]] = (fields[student[student.length - 1]] || 0) + 1;
-        });
-        out.push('This is the list of our students');
-        out.push(`Number of students: ${students.length}`);
-        for (const field in fields) {
-          if (field) {
-            // Filter just the firts names of the students
-            const result = students
-              .filter((std) => std[std.length - 1] === field)
-              .map((el) => el[0]);
-            out.push(`Number of students in ${field}: ${result.length}. List: ${result.join(', ')}`);
+
+    if (process.argv[2] && fs.existsSync(process.argv[2])) {
+      let students = [];
+      const out = [];
+      fs.readFile(process.argv[2], (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          students = data.toString().split('\n').map((elem) => elem.split(','));
+          students = students.slice(1, students.length - 1); // Array of array of students
+          const fields = {}; // All fields availables
+          students.forEach((student) => {
+            fields[student[student.length - 1]] = (fields[student[student.length - 1]] || 0) + 1;
+          });
+          // out.push('This is the list of our students');
+          out.push(`Number of students: ${students.length}`);
+          for (const field in fields) {
+            if (field) {
+              // Filter just the firts names of the students
+              const result = students
+                .filter((std) => std[std.length - 1] === field)
+                .map((el) => el[0]);
+              out.push(`Number of students in ${field}: ${result.length}. List: ${result.join(', ')}`);
+            }
           }
         }
-      }
-      res.write(`${out.join('\n')}`);
-      res.end();
-    });
+        res.write(`${out.join('\n')}`);
+        res.end();
+      });
+    } else {
+      res.write('This is the list of our students ');
+      res.end('Cannot load the database');
+    }
   } else {
     res.end('Invalid Request!');
   }
