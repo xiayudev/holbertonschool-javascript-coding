f665f64cdf1a047adfcb442cdc3a77a06f23a4e8
xiayudev <jctuesta7@gmail.com>
2023-10-08 16:22:39 -0500

    feat: correct output

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,7 +1,6 @@
 export class AppController {
   static getHomepage(request, response) {
-    response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
-    response.send('Hello Holberton School!');
+    response.status(200).send('Hello Holberton School!');
   }
 }
--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,11 +6,9 @@ export class StudentsController {
   static getAllStudents(request, response) {
     if (fs.existsSync(database)) {
       readDatabase(database).then((data) => {
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/plain');
         let totalStudents = 0;
         const out = [];
-
+        
         for (const field in data) {
           if (field) {
             totalStudents += data[field].length;
@@ -19,7 +17,8 @@ export class StudentsController {
         }
         out.unshift(`Number of students: ${totalStudents}`);
         out.unshift('This is the list of our students');
-        response.send(`${out.join('\n')}`);
+        response.setHeader('Content-Type', 'text/plain');
+        response.status(200).send(`${out.join('\n')}`);
       }).catch((error) => {
         response.statusCode = 500;
         response.setHeader('Content-Type', 'text/plain');
@@ -34,16 +33,14 @@ export class StudentsController {
   static getAllStudentsByMajor(request, response) {
     const majors = ['CS', 'SWE'];
     if (majors.includes(request.params.major)) {
-      response.statusCode = 200;
-      response.setHeader('Content-Type', 'text/plain');
       if (fs.existsSync(database)) {
         readDatabase(database).then((data) => {
           const show = data[request.params.major].join(', ');
-          response.send(`List: ${show}`);
-        }).catch((error) => {
-          response.statusCode = 500;
           response.setHeader('Content-Type', 'text/plain');
-          response.send(error.message);
+          response.status(200).send(`List: ${show}`);
+        }).catch((error) => {
+          response.setHeader('Content-Type', 'text/plain');
+          response.status(500).send(error.message);
         });
       } else {
         response.setHeader('Content-Type', 'text/plain');
